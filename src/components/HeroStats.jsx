a3752f8a1f686dee9be703bf3a3a338f5ea731d5
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-04-30 22:51:44 +0530

    json server

--- src/components/HeroStats.jsx
+++ src/components/HeroStats.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
-function HeroStats() {
+function HeroStats({ data }) {
   return (
     <div className="flex justify-around">
-      <DashDataComponent></DashDataComponent>
-      <DashDataComponent></DashDataComponent>
-      <DashDataComponent></DashDataComponent>
+      <DashDataComponent data={data}></DashDataComponent>
+      <DashDataComponent data={data}></DashDataComponent>
+      <DashDataComponent data={data}></DashDataComponent>
     </div>
   );
 }
 
-const DashDataComponent = () => {
+const DashDataComponent = ({ data }) => {
+  console.log(data[0].title);
   return (
     <div className="stats-container flex justify-around">
       <div className="stats-left-container flex gap-8 bg-gray-200">
-        <div className="text-container">
-          <div className="active-users-heading">Total Active Users</div>
-          <div className="active-users-percentage">+2.6%</div>
-          <div className="active-users-number">18,765</div>
-        </div>
+        {/* <div className="text-container">
+          <div className="active-users-heading">{data[0].title}</div>
+          <div className="active-users-percentage">{data[0].percent}</div>
+          <div className="active-users-number">{data[0].totalNumber}</div>
+        </div> */}
         <div className="graph-container items-center  h-full">graph</div>
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,34 @@ import Hero from './Hero';
 import HeroGraph from './HeroGraph';
 import HeroStats from './HeroStats';
 import Navbar from './Navbar';
-
+import api from '../api/apiData';
+import { useState, useEffect } from 'react';
 function Main() {
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    const response = await api.get('/posts');
+    setData(response.data);
+    return response.data;
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      const gettingData = await fetchData();
+      if (gettingData) {
+        setData(gettingData);
+      }
+
+      return gettingData;
+    };
+    getData();
+  }, []);
+  console.log(data);
   return (
     <div className="Main w-full p-2 h-full">
       <Navbar />
       <Hero />
-      <HeroStats />
+      <HeroStats data={data} />
       <HeroGraph />
     </div>
   );
