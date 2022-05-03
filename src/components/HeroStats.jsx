61e831e9845000dee4b4cbd542dff7a4725b55fe
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-05-03 14:02:13 +0530

    completed✅

--- src/components/HeroStats.jsx
+++ src/components/HeroStats.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import BarChartStats from '../Charts/BarChartStats';
 
-function HeroStats({ data }) {
+function HeroStats() {
   return (
     <div className="flex justify-between gap-8 p-4 flex-wrap">
-      <DashDataComponent data={data}></DashDataComponent>
-      <DashDataComponent data={data}></DashDataComponent>
-      <DashDataComponent data={data}></DashDataComponent>
+      <DashDataComponent></DashDataComponent>
+      <DashDataComponent></DashDataComponent>
+      <DashDataComponent></DashDataComponent>
     </div>
   );
 }
 
-const DashDataComponent = ({ data }) => {
-  console.log(data[0].percent);
+const DashDataComponent = () => {
+  console.log();
   return (
     <div className="stats-container flex justify-around ">
       <div className="stats-left-container flex gap-8 bg-white  shadow-lg px-6 py-4 rounded-lg justify-center">
         <div className="text-container">
           <div className="active-users-heading capitalize font-semibold text-xs">
-            {data[0].title}
+            {'Total Active Users'}
           </div>
           <div className="active-users-percentage items-center p-1">
-            +{data[0].percent}
+            +{'2.6%'}
           </div>
           <div className="active-users-number text-2xl font-bold">
-            {Number(data[0].totalNumber).toLocaleString()}
+            {Number('18786').toLocaleString()}
           </div>
         </div>
         <div className="graph-container  items-center h-full flex">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,40 +3,15 @@ import Hero from './Hero';
 import HeroGraph from './HeroGraph';
 import HeroStats from './HeroStats';
 import Navbar from './Navbar';
-import api from '../api/apiData';
-import { useState, useEffect } from 'react';
 
 import Links from './Links';
 
 function Main() {
-  const [data, setData] = useState([{}]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchData = async () => {
-    const response = await api.get('/posts');
-    setData(response.data);
-    setLoading(false);
-    return response.data;
-  };
-
-  useEffect(() => {
-    const getData = async () => {
-      const gettingData = await fetchData();
-      if (gettingData) {
-        setData(gettingData);
-      }
-
-      return gettingData;
-    };
-    getData();
-  }, []);
-  console.log(data);
-  if (loading) return <h1>Loading...</h1>;
   return (
     <div className="Main w-full p-2 h-full border-l-2 border-grey-200">
       <Navbar />
       <Hero />
-      <HeroStats data={data} />
+      <HeroStats />
       <HeroGraph />
       <Links />
     </div>
